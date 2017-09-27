dffa69829163df484ca1f08033c7a1c8bf08c8c1
Iceapinan <ice.ap2543@gmail.com>
2017-09-27 16:28:46 +0700

    Integrate CoreML and Vision Frameworks

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
 class CameraVC: UIViewController {
 
     var captureSession: AVCaptureSession!
-    var cameraOutput: AVCaptureOutput!
+    var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var photoData: Data?
     
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
-    
     @IBOutlet weak var flashButton: RoundedShadowButton!
     @IBOutlet weak var identificationLabel: UILabel!
     @IBOutlet weak var confidenceLabel: UILabel!
-    
     @IBOutlet weak var cameraView: UIView!
-    
     @IBOutlet weak var roundedLabelView: RoundedShadowView!
     
     override func viewDidLoad() {
@@ -37,6 +37,8 @@ class CameraVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = .hd1920x1080
         
@@ -54,15 +56,57 @@ class CameraVC: UIViewController {
                 previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 cameraView.layer.addSublayer(previewLayer!)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
         } catch {
             debugPrint(error)
         }
-        
     }
-
     
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+//        let previewPixelType = settings.__availablePreviewPhotoPixelFormatTypes.first!
+//        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        for classification in results {
+            if classification.confidence < 0.5 {
+                self.identificationLabel.text = "I'm not sure what this is. Please try again."
+                self.confidenceLabel.text = ""
+                break
+            } else {
+                self.identificationLabel.text = classification.identifier
+                self.confidenceLabel.text = "CONFIDENCE: \(Int(classification.confidence * 100))%"
+                break
+            }
+        }
+    }
+}
 
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!, options: [:])
+                try handler.perform([request])
+            } catch {
+                debugPrint(error)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+    }
 }
 
